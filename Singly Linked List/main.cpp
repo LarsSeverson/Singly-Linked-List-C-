2ac844b5d8668c6f41e637cf8f1bd8a2bbef06c5
Lars Severson <larsseverson1@gmail.com>
2022-08-19 22:23:38 -0700

    Initial Commit

---
+++ Singly Linked List/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Singly Linked List
+//
+//  Created by Lars Severson on 8/19/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
